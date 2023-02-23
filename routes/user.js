97753fef00c61080584f66b45521f4d321201be6
Chris Dias <105737889+chrisdias2311@users.noreply.github.com>
2023-02-23 09:30:23 +0530

    User & Client Authentication & User Validation done

--- routes/user.js
+++ routes/user.js
@@ -13,22 +13,6 @@ const URL = `localhost:5000`
 
 
 
-// Signup
-// router.post('/signup', (req, res) => {
-//     const newUser = new User({
-//         email: req.body.email,
-//         firstname: req.body.firstname,
-//         lastname: req.body.lastname,
-//         password: req.body.password,
-//         idCard: req.body.idCard
-//     });
-
-//     newUser.save()
-//         .then(newUser => res.json(newUser))
-//         .catch(err => res.status(400).json(err));
-// })
-
-
 
 //Rigister (alternative to Signup)
 router.post("/register", multer.upload.single("file"), async (req, res) => {
@@ -48,55 +32,56 @@ router.post("/register", multer.upload.single("file"), async (req, res) => {
             class: req.body.class,
             password: req.body.password,
             IDcard: `${URL}/api/image/${req.file.filename}`,
-            validity: 'Yes', //
+            validity: 'No', //Default validity of user is no 
         });
 
         const saved = await newUser.save();
         // res.send(newUser);
-        res.send("Your Request was sent Successfully!")
+        res.send(newUser)
 
 
     } catch (error) {
         console.log(error);
         res.send(error)
     }
-
-    // console.log("This is req body", req.body);
-    // const user = await User.findOne({ email: req.body.email })
-    // if (user) return res.status(400).send("Account already exists");
-
-    // const newUser = await User.create(req.body);
-    // // res.status(201).send(newUser);
-
-    // jwt.sign({ newUser }, secretKey, { expiresIn: '2h' }, (err, token) => {
-    //     if (err) {
-    //         console.log(err);
-    //         res.send(err);
-    //     }
-    //     res.status(201).send({ newUser, auth: token })
-    // })
 })
 
 
-router.post("/login", async (req, res) => {
-    if (req.body.password && req.body.email) {
+// router.post("/login", async (req, res) => {
+//     if (req.body.password && req.body.email) {
+//         let user = await User.findOne(req.body);
+//         if (user) {
+//             jwt.sign({ user }, secretKey, { expiresIn: '2h' }, (err, token) => {
+//                 if (err) {
+//                     console.log(err);
+//                     res.send(err);
+//                 }
+//                 res.send({ user, auth: token })
+//             })
+//         } else {
+//             res.send({ result: 'No user found' });
+//         }
+//     }
+// })
+
+router.post("/login", async(req, res) => {
+    try {
+        console.log("The request:", req.body)
         let user = await User.findOne(req.body);
-        if (user) {
-            jwt.sign({ user }, secretKey, { expiresIn: '2h' }, (err, token) => {
-                if (err) {
-                    console.log(err);
-                    res.send(err);
-                }
-                res.send({ user, auth: token })
-            })
-        } else {
-            res.send({ result: 'No user found' });
+        console.log(user);
+        if(user){
+            res.send(user);
+        }else{
+            res.send("No user found");
         }
+    } catch (error) {
+        res.send(error);
+        console.log(error);
     }
 })
 
-router.get('/unvalidusers', async (req, res) => {
 
+router.get('/invalidusers', async (req, res) => {
     try {
         let users = await User.find({ validity: 'No' });
         if (users.length > 0) {
@@ -109,23 +94,24 @@ router.get('/unvalidusers', async (req, res) => {
     }
 })
 
+router.put("/validateuser/:id", async(req, res)=> {
+    let result = await User.updateOne(
+        { _id: req.params.id },
+        {
+            $set: req.body
+        }
+    )
+    res.send(result);
+})
 
-
-
-// app.get("/products", async (req, resp) => {
-//     let products = await Product.find();
-//     if (products.length > 0) {
-//         resp.send(products)
-//     } else {
-//         resp.send({ result: "No products found" });
-//     }
-// })
-
-
-// app.delete("/product/:id", async (req, resp) => {
-//     const result = await Product.deleteOne({ _id: req.params.id });
-//     resp.send(result);
-//     console.log(result)
-// })
+router.put("/declineuser/:id", async(req, res)=> {
+    let result = await User.updateOne(
+        { _id: req.params.id },
+        {
+            $set: req.body
+        }
+    )
+    res.send(result);
+})
 
 module.exports = router;
