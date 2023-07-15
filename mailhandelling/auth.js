14446a2bf7143895d17897554fb5e1e28c6f24d4
Chris <chrisdias2311@gmail.com>
2023-07-16 01:32:27 +0530

    Changed Email verification

--- mailhandelling/auth.js
+++ mailhandelling/auth.js
@@ -10,8 +10,8 @@ async function sendOtp(otp, email) {
             port: 587,
             secure: false,
             auth: {
-                user: "jasonsampy88@gmail.com", //add acc
-                pass: "pohdsrsqvrvohkzv"//change
+                user: "uniexsfit@gmail.com", //add acc
+                pass: "joiaibielixfqfze"//change
             }
         }
     );
